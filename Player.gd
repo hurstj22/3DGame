e8c7c9e474dc7663ac8e4ff1709d3d1baba634ed
yamesboy <89492659+hurstj22@users.noreply.github.com>
2022-10-10 18:51:50 -0700

    making the player work

--- Player.gd
+++ Player.gd
@@ -7,38 +7,60 @@ export var jump_impulse = 20.0
 var gravity = -75.0
 var max_gravity = -150
 var velocity = Vector3.ZERO
+var mouseDelta : Vector2 = Vector2()
 var acceleration = 16
 var cam_sensitivity = 0.01
+
+# cam look
+var minLookAngle : float = -90.0
+var maxLookAngle : float = 90.0
+var lookSensitivity : float = 10.0
+
 # Get the gravity from the project settings to be synced with RigidDynamicBody nodes.
 #var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 onready var neck := $Neck
-onready var camera := $Neck/Camera
-
+onready var camera := $Camera
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	elif event.is_action_pressed("ui_cancel"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
-		if event is InputEventMouseMotion: #move and rotate the camera relative to the mouse
-			neck.rotate_y(-event.relative.x * cam_sensitivity) #in radians
-			camera.rotate_x(-event.relative.y * cam_sensitivity)
+	#if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+	#	if event is InputEventMouseMotion: #move and rotate the camera relative to the mouse
+			#neck.rotate_y(-event.relative.x * cam_sensitivity) #in radians
+			#camera.rotate_x(-event.relative.y * cam_sensitivity)
 			#limit the camera, otherwise the neck will rotate backwards and upside down
-			camera.rotation.x = clamp(camera.rotation.x, deg2rad(-30), deg2rad(60))
+			#camera.rotation.x = clamp(camera.rotation.x, deg2rad(-30), deg2rad(60))
 
+func _process(delta):
+	
+# rotate the camera along the x axis
+	camera.rotation_degrees.x -= mouseDelta.y * lookSensitivity * delta
+#clamp camera x rotation axis
+	camera.rotation_degrees.x = clamp(camera.rotation_degrees.x, minLookAngle, maxLookAngle)
+# rotate the player along their y-axis
+	rotation_degrees.y -= mouseDelta.x * lookSensitivity * delta
+	#reset the mouseDelta vector
+	mouseDelta = Vector2()
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		mouseDelta = event.relative
 
 func _physics_process(delta: float) -> void:
 	var direction = Vector3.ZERO
+	velocity.x = 0
+	velocity.z = 0
 
 	# Get the input direction and handle the movement/deceleration.
 	if Input.is_action_pressed("right"):
-		direction.x -= 1
-	if Input.is_action_pressed("left"):
 		direction.x += 1
+	if Input.is_action_pressed("left"):
+		direction.x -= 1
 	if Input.is_action_pressed("back"):
-		direction.z -= 1
+		direction.y += 1
 	if Input.is_action_pressed("forward"):
-		direction.z += 1
+		direction.y -= 1
 	
 	# Handle Jump.
 	if Input.is_action_pressed("jump") and is_on_floor():
@@ -48,11 +70,20 @@ func _physics_process(delta: float) -> void:
 		direction = direction.normalized()
 		#$Neck.look_at(translation + direction, Vector3.UP)
 	
+	var forward = global_transform.basis.z
+	var right = global_transform.basis.x
+	
+	print("forward " + str(forward))
+	print("right " + str(right))
+	
+	
+	var relativeDir = (forward*direction.y + right*direction.x) #calc new relative dir
+	print(relativeDir)
 	#calc what the game thinks is forwards based on the neck
 	#direction = (neck.transform.basis * Vector3(direction.x, 0, direction.y)).normalized()
 	
-	velocity.x = lerp(velocity.x, direction.x*speed, acceleration*delta)
-	velocity.z = lerp(velocity.z, direction.z*speed, acceleration*delta)
+	velocity.x = lerp(relativeDir.x, relativeDir.x*speed, acceleration*delta)
+	velocity.z = lerp(relativeDir.z, relativeDir.z*speed, acceleration*delta)
 	
 	#add gravity
 	velocity.y += gravity * delta
