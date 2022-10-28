e651d37907db514fed0cb792025801dd97e38212
g-marshak <marshak22@up.edu>
2022-10-27 18:10:41 -0700

    Play Safe Animation

--- Interactable/InteractableSafe.gd
+++ Interactable/InteractableSafe.gd
@@ -42,6 +42,7 @@ func _on_Keypad_correct_password():
 	locked = false
 	keypad.hide()
 	get_tree().paused = false
+	$AnimationPlayer.play("open")
 
 #keep safe locked, just hide the keypad
 func _on_Keypad_incorrect_password():
